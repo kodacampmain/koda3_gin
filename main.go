da667f80cef191a67cf51b7282ef9c8cdce2c610
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-09-02 11:48:56 +0700

    feat: trying gin

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// inisialisasi engine gin
+	router := gin.Default()
+	// setup routing
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, Response{
+			Message: "Rute Salah",
+			Status:  "Rute Tidak Ditemukan",
+		})
+	})
+	// jalankan engine gin
+	router.Run(":8080")
+}
+
+type Response struct {
+	Message string
+	Status  string
+}
