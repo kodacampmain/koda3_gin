31655472ae30fc8061a11d82630872ff3da08854
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-09-04 12:58:02 +0700

    feat: creating hash package

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
-	"github.com/kodacampmain/koda3_gin/internal/configs"
-	"github.com/kodacampmain/koda3_gin/internal/routers"
+	"github.com/kodacampmain/koda3_gin/pkg"
 
 	// "github.com/joho/godotenv"
 	_ "github.com/joho/godotenv/autoload"
@@ -20,22 +19,43 @@ func main() {
 
 	// inisialisasi db
 	// psql string: postgres://username:password@host:port/namadb
-	db, err := configs.InitDB()
-	if err != nil {
-		log.Println("Failed to connect to database\nCause: ", err.Error())
-		return
-	}
-	defer db.Close()
+	// db, err := configs.InitDB()
+	// if err != nil {
+	// 	log.Println("Failed to connect to database\nCause: ", err.Error())
+	// 	return
+	// }
+	// defer db.Close()
 	// testing koneksi db
 
-	if err := configs.TestDB(db); err != nil {
-		log.Println("Ping to DB failed\nCause: ", err.Error())
-		return
-	}
-	log.Println("DB Connected")
+	// if err := configs.TestDB(db); err != nil {
+	// 	log.Println("Ping to DB failed\nCause: ", err.Error())
+	// 	return
+	// }
+	// log.Println("DB Connected")
 	// inisialisasi engine gin
-	router := routers.InitRouter(db)
+	// router := routers.InitRouter(db)
 	// client => (router => handler => repo => handler) => client
 	// jalankan engine gin
-	router.Run("localhost:3000")
+	// router.Run("localhost:3000")
+	hc := pkg.NewHashConfig()
+	hc.UseRecommended()
+
+	password := "koda"
+	hash, err := hc.GenHash(password)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	isMatch, err := hc.CompareHashAndPassword(password, hash)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(isMatch)
+	// hash, err = hc.GenHash(password)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// fmt.Println(hash)
 }
